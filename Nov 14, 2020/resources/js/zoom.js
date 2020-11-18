2e72f8cd62a63c4ac33709ffcef004118836ce9f
kyeung7 <kevin.yeung.1@uvm.edu>
2020-11-18 12:21:18 -0500

    More compatibility changes, added interactive bg game

---
+++ Nov 14, 2020/resources/js/zoom.js
@@ -0,0 +1,4 @@
+var scale='scale(0.6)';
+document.body.style.webkitTransform= scale; // Chrome, Opera, Safari
+document.body.style.webkitTransform= scale; // IE
+document.body.style.webkitTransform= scale; //General
